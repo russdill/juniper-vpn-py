ff08c51be0c467b682d9cd083968aa22b5cdcced
Chris Diamand <chris@diamand.org>
2020-07-03 08:30:53 +0100

    Document package manager dependency installation
    Add another section to README describing installation with
    distribution-supplied dependencies. This makes it possible to remove the
    warnings in the wrapper scripts.

--- tncc.py
+++ tncc.py
@@ -1,11 +1,6 @@
 #! /usr/bin/env python3
 
-import warnings
+from junipervpn import tncc
 
 if __name__ == '__main__':
-    warnings.warn('Please use the shim created by pip when installing the junipervpn package. If you use a virtualenv, it will be in PATH after activating it')
-
-    # Import after the warning, so the warning is the first thing seen rather
-    # than some ImportError due to some missing packages
-    from junipervpn import tncc
     tncc.main()
